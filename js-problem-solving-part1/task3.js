553f0959fa6bd0b43f66c70c1bed88587a28f9cb
sujon <jasujon570@gmail.com>
2025-08-11 02:21:54 +0600

    find the total vowels in a string

--- js-problem-solving-part1/task3.js
+++ js-problem-solving-part1/task3.js
@@ -8,7 +8,7 @@ Write a function to count the number of vowels in a string.
 function countVowels(str){
     const vowels = 'aeiouAEIOU';
     let count = 0;
-    
+
     for (const element of str) {
         if(vowels.includes(element)){
             count++;
@@ -19,4 +19,25 @@ function countVowels(str){
 
 const str = 'I love you';
 console.log(countVowels(str));
+
+
+console.log('--------check which vowels found in the string------------');
+
+
+function totalVowels(string){
+    const vowels = 'aeiouAEIOU';
+    let countVowels = 0;
+    let foundVowels = [];
+    for(const char of string){
+        if(vowels.includes(char)){
+            countVowels++;
+            foundVowels.push(char.toLowerCase());
+        }
+    }
+    console.log('Vowels found in the string:', foundVowels.join(', '));
+    return countVowels;
+}
+
+const getVowels = 'I love JavaScript';
+console.log(totalVowels(getVowels));
 
