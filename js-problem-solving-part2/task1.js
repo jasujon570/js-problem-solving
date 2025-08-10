c3df1fadc1bff0bb59726e0410a7c6729811080b
sujon <jasujon570@gmail.com>
2025-08-10 23:10:29 +0600

    code modified

--- js-problem-solving-part2/task1.js
+++ js-problem-solving-part2/task1.js
@@ -1,20 +1,18 @@
 /* Find the lowest number in the array below.
-    const heights2 = [167, 190, 120, 165, 137];
+    const findLowest = [167, 190, 120, 165, 137];
 */
 
-const numbersArr = [167, 190, 120, 165, 137, 10];
-numbersArr.push(4);
-function lowestNumber(numbers){
-        let minNumber = numbers[0];
-        for (const number of numbers) {
-            if(number < minNumber){
-                minNumber = number;
-            }
+const findLowest = [167, 190, 120, 165, 137];
+function LowestNumber(numbers){
+    let minNumber = numbers[0];
+    for (const number of numbers) {
+        // console.log(number);
+        if(number < minNumber){
+            minNumber = number;
         }
-        return minNumber;
     }
+    return minNumber;
+}
 
-
-
-const min = (lowestNumber(numbersArr));
-console.log('Lowest number is:',min);
+const result = LowestNumber(findLowest);
+console.log('The lowest nunber is:', result);
