163c8fc151e89dfecb24ac8939da20c4d35ddbbd
sujon <jasujon570@gmail.com>
2025-08-11 02:09:09 +0600

    find vowels in a string

--- js-problem-solving-part1/task3.js
+++ js-problem-solving-part1/task3.js
@@ -2,3 +2,21 @@
  * ### Task-3: 
 Write a function to count the number of vowels in a string.
  */
+
+
+
+function countVowels(str){
+    const vowels = 'aeiouAEIOU';
+    let count = 0;
+    
+    for (const element of str) {
+        if(vowels.includes(element)){
+            count++;
+        }
+    }
+    return count;
+}
+
+const str = 'I love you';
+console.log(countVowels(str));
+
