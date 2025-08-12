79ec15ac5b90dbda21d7193df0946ea5a543b414
sujon <jasujon570@gmail.com>
2025-08-12 16:08:58 +0600

    validate the code

--- js-conceptual-session-problem-solving/task2.js
+++ js-conceptual-session-problem-solving/task2.js
@@ -17,20 +17,23 @@ const myArray = [1, 2, 3];
 const result = sumOfArray(myArray);
 // console.log(result);
 
-
 // using simple for loop
 
 function sumThisArray(theArray) {
-//   console.log(theArray);
-    let sum = 0;
-    for(let i = 0; i < theArray.length; i++){
-        sum += theArray[i];
+  if (Array.isArray(theArray) !== true) {
+    return "Invalid";
+  }
+  //   console.log(theArray);
+  let sum = 0;
+  for (let i = 0; i < theArray.length; i++) {
+    if (typeof i !== "number") {
+      return "Invalid";
     }
-    return sum;
+    sum += theArray[i];
+  }
+  return sum;
 }
 
-
-const thisIsArray = [12, 25, 36, 445, 14, 7];
+const thisIsArray = 10;
 const output = sumThisArray(thisIsArray);
 console.log(output);
-
